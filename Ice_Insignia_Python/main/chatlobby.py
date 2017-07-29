be434c4adf6eb1086ad433fda97dfdd166ec2207
Daniel Flynt <flyntdw@unc.edu>
2017-07-29 00:47:32 -0400

    Add files via upload

--- Ice_Insignia_Python/main/chatlobby.py
+++ Ice_Insignia_Python/main/chatlobby.py
@@ -1,26 +1,67 @@
 from flask import Blueprint, render_template, request, session
-from flask_socketio import SocketIO, emit, send, join_room, leave_room
+from flask_socketio import join_room, emit, rooms
 from .. import socketio
 
-clients = []
+clientsDict = {} #dictionary instead of list for easy class information access. O(1)
+			 	 #key = username, value = Client(username, socket ID)
 
-lobby = Blueprint('lobby', __name__,
-					  template_folder='templates')
+clientsList = [] #list for sending connected usernames to new client
+				 #because it's not easy to send a dictionary through json
 
-@socketio.on('connect', namespace='/lobby')
-def connect():
-	print("Request.namespace: ", end="")
-	print(request.sid)
-	clients.append(request.sid)
-	print(clients)
-	room = session.get('room')
-	join_room(room)
-	emit('text', {'msg': "New user"})
-	print(room)
-	#emit('message', {'data': "Hello"})
+@socketio.on('joined', namespace='/lobby')
+#Previously had a TypeError saying I wasn't providing the message positional
+#argument but I was. Still confused on that.
+def joinServer(message):
+	username = message['msg'] #username of new client
+	clientsDict[username] = Client(username, request.sid)
+	clientsList.append(username)
+	join_room(username) #newly connected client has their own 
+						#room for sending challenges/specific messages
+						#Example below in the emit('showUsersList') line
 
-@socketio.on('rec_message')
-def receive_message(message):
-	print("Received message: " + message)
-	room = session.get('room')
-	emit('text', {'msg': session.get('name') + ": " + message['msg']}, room=room)
+	emit('printmessage', {'msg': username + " connected to server."}, broadcast=True)
+
+	#add new client to username-list of all previously connected clients
+	emit('addNewUser', {'msg': username}, broadcast=True)
+
+	#send already-connected users to the new client
+	emit('showUsersList', clientsList, room=username)
+
+@socketio.on('send_message', namespace='/lobby')
+def sendMessage(message):
+	print("Received message: " + message['msg'])
+
+	emit('printmessage', message, broadcast=True)
+
+@socketio.on('challenge', namespace='/lobby')
+def sendChallenege(message):
+	players = message['msg'].split(",")
+	receiver = players[0]
+	sender = players[1]
+	print(sender + " sent a challenge to " + receiver)
+
+	#send a challenge message to the one being challenged
+	emit('receiveChallenge', {'msg': sender}, room=receiver)
+
+#disconnectUser works but disconnect doesn't?
+@socketio.on('disconnectUser', namespace='/lobby')
+def disconnectClient(message):
+	username = message['msg']
+	print(username + " has disconnected.")
+	del clientsDict[username]
+	clientsList.remove(username)
+
+	emit('printmessage', {'msg': username + " has disconnected."}, broadcast=True)
+
+	emit('removeUser', {'msg': username}, broadcast=True)
+
+
+class Client:
+	def __init__(self, username, socketID):
+		self.username = username
+		self.socketID = socketID
+
+	def getUsername():
+		return self.username
+	def getSocketID():
+		return self.socketID74  
