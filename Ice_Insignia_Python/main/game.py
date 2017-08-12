a5caca7e04abd65db3a4b824d680f1f2f1d7ea3c
Daniel Flynt <flyntdw@unc.edu>
2017-08-11 22:23:50 -0400

    Add files via upload

--- Ice_Insignia_Python/main/game.py
+++ Ice_Insignia_Python/main/game.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, session
 from flask_socketio import join_room, emit, rooms
 from .. import socketio
+import random
 
 clientsList = []
 @socketio.on('joinedGameLobby', namespace='/loadout')
@@ -16,8 +17,7 @@ def selectChar(message):
     user = info[0] #user to send message to
     htmlElem = info[1] #element that will get the picture
     fileLoc = info[2] #source of the picture
-    emit('setCharChoice', {'html': htmlElem, 'fileLoc': fileLoc},
-                            room=user)
+    emit('setCharChoice', {'html': htmlElem, 'fileLoc': fileLoc}, room=user)
 
 @socketio.on('selectItem', namespace='/loadout')
 def selectItem(message):
@@ -25,11 +25,22 @@ def selectItem(message):
     user = info[0]
     htmlElem = info[1]
     fileLoc = info[2]
-    emit('setCharChoice', {'html': htmlElem, 'fileLoc': fileLoc}, 
-                            room=user)
+    emit('setItemChoice', {'html': htmlElem, 'fileLoc': fileLoc}, room=user)
+
 @socketio.on('readyUp', namespace='/loadout')
 def readyUp(message):
-    pass
+    print("in readyup")
+    playerToGetMessage = message['msg']
+    emit('readyUp', {'msg': "start"}, room=playerToGetMessage)
+
+@socketio.on('startCountdown', namespace='/loadout')
+def sendCountdownMessage(message):
+    mapNumber = random.randint(1, 3)
+    info = message['msg'].split(",")
+    playerOne = info[0]
+    playerTwo = info[1]
+    emit('startCountdown', {'msg': mapNumber}, room = playerOne)
+    emit('startCountdown', {'msg': mapNumber}, room = playerTwo)
 
 @socketio.on('sendMove', namespace='/game')
 def sendMove(message):
@@ -40,5 +51,3 @@ class Game:
     def __init__(self, clientone, clienttwo):
         self.playerone = clientone
         self.playertwo = clienttwo
-
-    
